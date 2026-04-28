d10f0c93c7133252a456d9d049e4194f37c4ea09
neon_0039 <uranium124427@gmail.com>
2026-04-28 22:08:43 +0900

    Update bot.js

--- bot.js
+++ bot.js
@@ -575,7 +575,7 @@ ${config.characterSetting}
         console.log("本投稿が完了しました！");
     } catch (e) { console.error(e); }
 }
-
+    try{
     else { // ★ if (words.length > 0) に対応する else
         const post_content = "（タイムラインに材料がありません）";
         console.log(post_content);
