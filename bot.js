756b14a1672a824eaf368bcc2e676e82434e1b21
neon_0039 <uranium124427@gmail.com>
2026-04-30 11:56:56 +0900

    Update bot.js

--- bot.js
+++ bot.js
@@ -565,7 +565,7 @@ ${config.characterSetting}
                 // }
 
                 // 上限20,000件（各単語ごとのつながり上限）
-                if (brain[current].length > 100000) {
+                if (brain[current].length > 10000000) {
                     brain[current].shift();
                 }
             }
